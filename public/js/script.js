91cea9da67ec1bd4842638c0e4aaf4306a06eb39
KrinickiJ.V.A <krinickij.v.a@gmail.com>
2022-02-10 23:17:19 +0300

    structure init

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+const goods = [
+  {artucule:1, title: 'Shirt', price: 150 },
+  {artucule:2, title: 'Socks', price: 50 },
+  {artucule:3, title: 'Jacket', price: 350 },
+  {artucule:4, title: 'Shoes',price: 350 },
+];
+
+const renderGoodsItem = (title, price="нет в наличии",artucule) => 
+  `<div class="goods-item">
+  <div class="goods__photo">
+    место для фото товара
+  </div>
+  <h3>${title}</h3>
+  <p>${price}</p>
+  <button class="goods__buy" type="button" data-id =${artucule}>В корзину</button>
+  </div>`;
+
+
+const renderGoodsList = (list = []) => {
+  let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.artucule));
+  document.querySelector('.goods-list').innerHTML = goodsList.join('');
+}
+
+renderGoodsList(goods);
